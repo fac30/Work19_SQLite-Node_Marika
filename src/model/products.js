2707ae942e264e1ad1008e43742a860ab13d2c75
MarikaBBB <mbertelli012@gmail.com>
2024-10-14 20:47:47 +0100

    completed challenge 1

--- src/model/products.js
+++ src/model/products.js
@@ -1,1 +1,20 @@
+const { listProducts } = require("../../solution/model/products.js");
 const db = require("../database/db.js");
+
+function listProducts() {
+    db.prepare(/*sql*/ `
+    SELECT
+    id,
+    product_name AS name, 
+    quantity_per_unit, 
+    unit_price, 
+    units_in_stock, 
+    units_on_order
+    FROM products
+    `);
+}
+
+
+
+
+module.exports = {listProducts};
