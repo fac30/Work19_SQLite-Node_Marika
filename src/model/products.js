d12d7fd7be8b17c93e4e5315d5ba63428bde842a
MarikaBBB <mbertelli012@gmail.com>
2024-10-14 21:42:12 +0100

    finished challenge 4

--- src/model/products.js
+++ src/model/products.js
@@ -1,42 +1,42 @@
-const { listProducts } = require("../../solution/model/products.js");
 const db = require("../database/db.js");
 
 function listProducts() {
-    const select_products = db.prepare(/*sql*/ `
+  const select_products = db.prepare(/*sql*/ `
     SELECT
-    id,
-    product_name AS name, 
-    quantity_per_unit, 
-    unit_price, 
-    units_in_stock, 
-    units_on_order
+      id,
+      product_name AS name,
+      quantity_per_unit,
+      unit_price,
+      units_in_stock,
+      units_on_order
     FROM products
-    `);
-    return select_products.all();
+  `);
+  return select_products.all();
 }
 
 function searchProducts(string) {
-    const search_products = db.prepare(/*sql*/ `
+  const search_products = db.prepare(/*sql*/ `
     SELECT
-    id,
-    product.name AS name
+      id,
+      product_name AS name
     FROM products
     WHERE product_name LIKE ?
-    `);
-    return search_products.all(`%${string}%`)
-
-} 
+  `);
+  return search_products.all(`%${string}%`);
+}
 
 function getProduct(id) {
-    const get_products = db.prepare(/*sql*/ `
-        SELECT
-        id,
-        product_name AS name
-        FROM products
-        WHERE id = ?
-        `);
-        return search_products.get(id);
-};
+  const get_product = db.prepare(/*sql*/ `
+    SELECT
+      p.id,
+      p.product_name AS name,
+      c.name AS category_name,
+      c.description AS category_description
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE p.id = ?
+  `);
+  return get_product.get(id);
+}
 
-
-module.exports = {listProducts, searchProducts, getProduct};
+module.exports = { listProducts, searchProducts, getProduct };
